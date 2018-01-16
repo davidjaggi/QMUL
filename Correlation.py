b89d9322d67f2a4e330b5ccd12da77dff0bd85be
davidjaggi <davidjaggi@users.noreply.github.com>
2018-01-16 20:10:06 +0000

    Added to correlation

--- Correlation.py
+++ Correlation.py
@@ -2,11 +2,38 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+import datetime
 
-df = pd.read_excel(open('Data/Correlation Data.xlsx','rb'), sheet_name= 'Correlation Static')
+# Read the data
+df = pd.read_excel(open('Data/Correlation Data.xlsx','rb'),
+                   sheet_name= 'Correlation Static',
+                   index_col='Date')
+# Index to datetime
+df.index = pd.to_datetime(df.index)
+# Plot the data
 df.head()
+# Remove NAs
 df = df.dropna()
-df.head()
-list(df)
+# Check for NAs
 df.isnull().sum()
+# List names and correct names
+list(df)
+names = ['Brent','BNO','WTI','YXA1','RE1','USD Index','USD/CAD',
+         'USD/NOK','S&P 500','EuroStoxx50']
+
 ret = df.pct_change()
+ret.columns = names
+ret.head()
+ret1y = ret.loc['2017-01-16':'2018-01-30']
+ret6m = ret.loc['2017-07-16':'2018-01-30']
+ret3m = ret.loc['2017-11-16':'2018-01-30']
+
+corr1y = ret1y.corr().dropna()
+corr6m = ret6m.corr().dropna()
+corr3m = ret3m.corr().dropna()
+# 1 Year
+sns.clustermap(corr1y,
+            annot=True,
+            cmap = 'RdYlGn',
+            xticklabels=corr.columns,
+            yticklabels=corr.columns)
